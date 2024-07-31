0ac01c52b4249b6846ebfe0a9eb5779caa7884c0
ericnjkim <ericnjkim@gmail.com>
2024-07-31 17:19:05 +1000

    gui now uses transcribe function with the  whisper library,  Qthread to prevent a freezing gui and a warning message dialog if the gui cannot transcribe due to missing parameters.

---
+++ scripts/qt/message_dialog.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QApplication
+
+
+UI_FILE = f"{os.path.dirname(__file__)}/ui/message_dialog.ui"
+
+
+class MessageDialog(QtWidgets.QDialog):
+
+    def __init__(self, text):
+        super().__init__()
+
+        uic.loadUi(UI_FILE, self)
+
+        self.label_message.setText(text)
+        self.btn_close.clicked.connect(self.close)
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    window = MessageDialog("mes")
+    window.show()
+    sys.exit(app.exec_())
---
+++ scripts/qt/transcription_thread.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QThread, pyqtSignal
+
+from scripts.core.functions_transcriptor import transcribe
+
+
+class TranscriptionThread(QThread):
+    output_ready = pyqtSignal(str)
+
+    def __init__(self, audio_file_path, transcriber_model_type):
+        super().__init__()
+        self.audio_file_path = audio_file_path
+        self.transcriber_model_type = transcriber_model_type
+
+    def run(self):
+        transcribed_text = transcribe(
+            self.audio_file_path,
+            self.transcriber_model_type)
+        self.output_ready.emit(transcribed_text)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    # set style
+    thread = TranscriptionThread(
+        "/home/ericnjkim/repo/transcription_tool/_mp3/2023_05_01.mp3",
+        "tiny").start()
+    print((thread))
+    sys.exit(app.exec_())
